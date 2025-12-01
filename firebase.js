7972dfa0e579842c06bd35e5caade2c68144c9f4
CapacitacionKeken <granjasgpm@gmail.com>
2025-12-01 10:23:16 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 // firebase.js - pega aquí tu configuración de Firebase si la usarás.
 // Si no deseas/puedes usar Firebase, el sistema caerá en un 'modo demo' con localStorage.
 (function(){
-  */
+    /*
   window.useFirebase = false; // Cambia a true si configuras Firebase
   // Si quieres usar Firebase, pega aquí tu configuración y coloca useFirebase = true
   // Ejemplo:
-  
+  */
   window.useFirebase = true;
   const firebaseConfig = {
     apiKey: "AIzaSyDpYfb5HkFvdo-obK0Cju7Xwkda1TFpKPs",

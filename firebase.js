4e8006195fab7430ab3fda82e4252bbd20d5617c
CapacitacionKeken <granjasgpm@gmail.com>
2025-12-01 12:28:19 -0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,16 +1,16 @@
 // firebase.js - pega aquí tu configuración de Firebase si la usarás.
 // Si no deseas/puedes usar Firebase, el sistema caerá en un 'modo demo' con localStorage.
 (function(){
-    /*
-  window.useFirebase = false; // Cambia a true si configuras Firebase
-  // Si quieres usar Firebase, pega aquí tu configuración y coloca useFirebase = true
-  // Ejemplo:
-  */
+
   window.useFirebase = true;
   const firebaseConfig = {
-    apiKey: "AIzaSyDpYfb5HkFvdo-obK0Cju7Xwkda1TFpKPs",
-    authDomain: "usuarios-da6ee.firebaseapp.com",
-    projectId: "usuarios-da6ee",
+  apiKey: "AIzaSyDpYfb5HkFvdo-obK0Cju7Xwkda1TFpKPs",
+  authDomain: "usuarios-da6ee.firebaseapp.com",
+  projectId: "usuarios-da6ee",
+  storageBucket: "usuarios-da6ee.firebasestorage.app",
+  messagingSenderId: "587797793570",
+  appId: "1:587797793570:web:ddb3696b13398b728b337b",
+  measurementId: "G-MMPPGM6H40"
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
